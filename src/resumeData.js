44c6141d0f87d273ecdd404159f22e88cfc53c8c
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-09-23 23:22:43 -0400

    base code

---
+++ src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Alex Kichigin",
+    "role": "Frontend / Backend Developer || UNCC Full Stack Student",
+    "linkedinId":"https://www.linkedin.com/in/alex-kichigin-294706161/",
+    "skypeid": "lol who uses skype anymore",
+    "roleDescription": "I find that my passion rests in learning new things as often as possible. I take on new challenges as much as I can in order to highten my skills in as much areas as possible!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/alex-kichigin-294706161/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/akish777",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"404",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I tend to try my hardest to reach goals that normally I wouldn't be able to. For example a Full Stack Developer class at UNCC while juggling a job, social life and instrument practice!",
+    "address":"3121 Spooner Street",
+    "website":"https://akish777.github.io/Week-2-HW/",
+    "education":[
+      {
+        "UniversityName":"UNCC",
+        "specialization":"Fullstack web Developing",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2021",
+        "Achievements":"Portfolio"
+      },
+      {
+        "UniversityName":"Butler High School",
+        "specialization":"Software",
+        "MonthOfPassing":"February",
+        "YearOfPassing":"2019",
+        "Achievements":"Passing with good grades and collecting my diploma"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Carolina Wood Flooring",
+        "specialization":"Helper",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2018",
+        "Achievements":"Being as useful as possible at any given moment"
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Special Project Management",
+        "description":"API Integration/HTML/CSS/JS",
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://tloyzelle.github.io/Project-Marvel/"
+      },
+      {
+        "name":"Cyber-Security",
+        "description":"Secure Password Generation/Web Security",
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://pedrazajulian.github.io/PasswordGenerator/"
+      },
+      {
+        "name":"pCDP Systems",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png",
+        "url": "https://protected-gorge-09109.herokuapp.com/"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"100% never misses a shot, guaranteed",
+        "name":"Michael Scott"
+      },
+      {
+        "description":"Nobody better for the job",
+        "name":"Jim Halpert"
+      },
+      {
+        "description":"Awesome dude!",
+        "name":"Kevin Malone"
+      },
+      {
+        "description":"He is competent. Also above average.",
+        "name":"Dwight K. Schrute"
+      }
+    ]
+  }
+  
+  export default resumeData
